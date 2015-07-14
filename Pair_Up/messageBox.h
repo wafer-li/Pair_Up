730ecc9adfb808d29aff4bea2d1ccd41f22ac5a3
jushe <792988035@qq.com>
2015-07-14 23:57:35 +0800

    1.add messageBox for leaderboardMode 2.fix bug of leaderMode

---
+++ Pair_Up/messageBox.h
@@ -0,0 +1,12 @@
+#ifndef MESSAGEBOX_H
+#define MESSAGEBOX_H
+#include "graphics15.h"
+#include <string>
+
+/*This function return a pointer of string
+* If user close it, it return a 0 size string
+* The size maybe need to adjust;
+*/
+std::string messageBox();
+
+#endif
