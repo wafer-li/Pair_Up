5f93e783c145558ec996d2d8e7b2bbc7ec3262f4
Crepusculo <airfree1452@gmail.com>
2015-07-16 13:42:25 +0800

    Improve score system -- level up is esaier now

--- Pair_Up/Score.cpp
+++ Pair_Up/Score.cpp
@@ -79,7 +79,7 @@ int Score::getScore()
 //The Interface that support for front-end 
 void Score::changeScore(int num, int com)
 {
-	score += int((30 * pow(1.5, num - 3) * com) + 0.5);
+	score += int((50 * pow(1.75, num - 3) * com) + 0.5);
 }
 
 //PassScore
